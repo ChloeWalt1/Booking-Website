ba6eee17aaa6248204c16d56da610b2f07846806
ChloeWalt1 <125471864+ChloeWalt1@users.noreply.github.com>
2023-09-03 19:31:05 +0200

    final submission term 3 - booking
    booking website, astral odyssey

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ const trips = [
     "name": "Merry Christmas at Mercury",
     "price": 20000,
     "description": "Welcome aboard the extraordinary cruise to Mercury, the sizzling jewel of our solar system! Departing from Earth's orbit, our state-of-the-art spacecraft navigates the vast cosmic seas towards the closest planet to the Sun. Prepare to be mesmerized as we approach this scorching world, where temperatures soar to extreme levels.",
-    "image": "plant2.png",
+    "image": "spaceship.png",
     "duration": "7 days", // Add duration
     "destinations": "2 destinations", // Add amount of destinations
     "roundTrip": false // Add round trip
@@ -42,6 +42,24 @@ const trips = [
     "image": "plant5.png",
     "duration": "9 days", // Add duration
     "destinations": "2 destinations", // Add amount of destinations
+    "roundTrip": false // Add round trip
+  },
+  {
+    "name": "All around the MOONS",
+    "price": 30000,
+    "description": "Experience the ultimate adventure on our Cruise to Three Moons! Journey to Triton, Neptune's icy moon, explore Europa's alien landscapes, and set foot on Phobos, a moon of Mars. Witness celestial wonders and thrilling exploration on this extraordinary voyage through the cosmos.",
+    "image": "plant4.png",
+    "duration": "3 days", // Add duration
+    "destinations": "3 destinations", // Add amount of destinations
+    "roundTrip": true // Add round trip
+  },
+  {
+    "name": "Big Celestial Experience",
+    "price": 15000,
+    "description": "Embarking on a grand voyage to Mars, the Red Planet, promises an unforgettable adventure through the cosmos. Our state-of-the-art spacecraft will guide you on a journey to the fourth planet from the Sun, offering a chance to explore its mysteries and captivating landscapes.",
+    "image": "plant4.png",
+    "duration": "12 days", // Add duration
+    "destinations": "3 destinations", // Add amount of destinations
     "roundTrip": false // Add round trip
   }
 ];
@@ -101,7 +119,7 @@ function appendTripToContainer(trip) {
   let currentTrip = $("#trips-template").contents().clone();
 
   // Set the content for the trip card from the trips list
-  currentTrip.find(".jupiterImg").attr('src', 'assets/' + trip.image);
+  currentTrip.find(".jupiterImg").attr('src', 'assets/cruise ship tour/Jupiter.webp' + trip.image);
   currentTrip.find(".nameText").text(trip.name);
   currentTrip.find(".priceText").text('R' + trip.price);
   currentTrip.find(".trips-desc").text(trip.description);
@@ -194,3 +212,51 @@ function changeHeaderText() {
 
 // Add an event listener to run the function when the page finishes loading
 window.addEventListener("DOMContentLoaded", changeHeaderText);
+
+// checkout
+$(document).ready(function() {
+  // Add a click event listener to all elements with the class "remove-button"
+  $(".remove-button").click(function() {
+      // Find the closest <tr> element (table row) and remove it
+      $(this).closest("tr").remove();
+  });
+});
+
+// remove all button
+
+$(document).ready(function() {
+  // Select the button by its id
+  $("#remove-all-button").on("click", function() {
+    // Find the table body and remove all its children (rows)
+    $("table tbody").empty();
+  });
+});
+
+$(document).ready(function() {
+  // Select the button by its id
+  $("#purchaseBtn").on("click", function() {
+    // Show the "Successful Purchase" message
+    $("#purchaseMessage").show();
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const popupContainer = document.getElementById("popupContainer");
+  const closePopupBtn = document.getElementById("closePopupBtn");
+
+  // Function to show the popup
+  function showPopup() {
+      popupContainer.style.display = "flex"; // Show the popup
+  }
+
+  // Function to hide the popup
+  function hidePopup() {
+      popupContainer.style.display = "none"; // Hide the popup
+  }
+
+  // Event listener for showing the popup (e.g., when the "Successful Purchase" button is clicked)
+  document.getElementById("purchaseBtn").addEventListener("click", showPopup);
+
+  // Event listener for hiding the popup (e.g., when the "Close" button is clicked)
+  closePopupBtn.addEventListener("click", hidePopup);
+});
